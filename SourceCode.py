bec252faf90c880b708b33a2e4a0462167b32940
44Matt44 <matt123.3a@gmail.com>
2024-04-02 10:37:29 +0200

    Add files via upload

---
+++ SourceCode.py
@@ -0,0 +1,34 @@
+import os
+import collections
+import glob
+
+# Lista putanja do direktorija
+directories = [
+    "C:/Users/korisnik/Desktop/Machine17/Croatian_Invoices_snippets",
+    "C:/Users/korisnik/Desktop/Machine17/French_Invoices_snippets",
+    "C:/Users/korisnik/Desktop/Machine17/Spanish_Invoices_snippets"
+]
+
+# Funkcija za čitanje prvog reda i izračunavanje frekvencije znakova
+def calculate_frequencies(file_path):
+    with open(file_path, 'r', encoding='utf8') as file:
+        first_line = file.readline()
+        return collections.Counter(first_line)
+
+# Rječnik za spremanje ukupnih frekvencija
+total_frequencies = collections.Counter()
+
+# Prođi kroz sve putanje i datoteke
+for directory in directories:
+    for file_path in glob.glob(os.path.join(directory, '*.txt')):
+        total_frequencies.update(calculate_frequencies(file_path))
+
+# Sortiraj rezultate
+sorted_frequencies = total_frequencies.most_common()
+
+# Ispis rezultata
+print("{:<10} {:<10}".format('Char', 'Freq'))
+print("{:<10} {:<10}".format('-----', '--------'))
+
+for char, freq in sorted_frequencies:
+    print("{:<10} {:<10}".format(repr(char), freq))
